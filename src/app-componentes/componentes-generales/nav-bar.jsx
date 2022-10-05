755b8c18e6bf6c69c695b7e88a6df40241738ff5
Joel Dario <joeldario9912@gmail.com>
2022-10-05 14:58:26 -0300

    agregue retardo a la visibilidad del navBar

--- src/app-componentes/componentes-generales/nav-bar.jsx
+++ src/app-componentes/componentes-generales/nav-bar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavList from './contenedores/nav-list';
 
 const NavBar = ({ ocultarMenu, estilos, estilosNavList, estilosLi, estilosLiOcultar }) => {
-  const estilosNavBarHeaderMovileVisible = 'absolute md:relative -translate-x-4 translate-y-48 md:translate-y-0 md:translate-x-0 w-full md:w-auto';
+  const estilosNavBarHeaderMovileVisible = 'absolute md:relative -translate-x-4 translate-y-48 md:translate-y-0 md:translate-x-0 w-full md:w-auto transition duration-200';
   const estilosNavBarHeaderMovileOculta  = 'absolute md:relative translate-y-48 md:translate-y-0 translate-x-full md:translate-x-0 w-full md:w-auto';
 
   let widthMovile = (window.innerWidth < 768) ? true : false;
